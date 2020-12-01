abb33a2b9be21b0a3fe70d4b120368f8efe75821
moonmovie <sosm8100@gmail.com>
2020-12-01 18:01:46 +0900

    Revise wearing

--- App.js
+++ App.js
@@ -1,52 +1,73 @@
-
-import React from 'react';
-import {Alert} from "react-native";
+import React from "react";
+import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
-import { Text, View, StyleSheet } from 'react-native';
-import Axios from 'axios';
-import Weather from './weather';
+import axios from "axios";
+import Weather from "./Weather";
 
-const API_KEY = "74efabcfc9a348ea7b1f3ff0afe5915f";
-//api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
+
 export default class extends React.Component {
-    state = {
-      isLoading: true
-    };
-    getWeather = async(latitude, longitude) => {
-        const {data} = await Axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
-        this.setState({
-          isLoading: false,
-          temp: data.main.temp,
-          temp_max: data.main.temp_max,
-          temp_min: data.main.temp_min,
-          name: data.name,
-          condition: data.weather[0].main,
-        });
-    };
-
-    getLocation = async() => {
-      try {
-        const response = await Location.requestPermissionsAsync();
-        console.log(response);
-        const { coords : {latitude, longitude} } = await Location.getCurrentPositionAsync();  
-        this.getWeather(latitude, longitude);
-        this.setState({isLoading: false});
-        
-     } catch (error) {
-        Alert.alert("Can't find you.", "set location permission");
-      } 
-      
-      
-    };
-    componentDidMount(){
-      this.getLocation();
+  state={
+    isLoding: true
+  };
+  getWeather = async (latitude, longitude) => {
+    
+    const { data:{main:{temp, temp_max, temp_min}, weather}} = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+    );
+    const value = ((temp_max+temp_min)/2)-2;
+    console.log(value,temp_max,temp_min);
+    const level =["tooHot","hot","tooWarm","warm","tooAutumn","autumn","tooCold","cold"];
+    let weatherStep="";
+    if(value>27){
+      weatherStep=level[0];
+    }else if(value<27 && value>22){
+      weatherStep=level[1];
+    }else if(value<22 && value>19){
+      weatherStep=level[2];
+    }else if(value<19 && value>16){
+      weatherStep=level[3];
+    }else if(value<16 && value>11){
+      weatherStep=level[4];
+    }else if(value<11 && value>8){
+      weatherStep=level[5];
+    }else if(value<8 && value>4){
+      weatherStep=level[6];
+    }else{
+      weatherStep=level[7];
     }
-    render(){
-      const { isLoading, temp, condition, temp_max, temp_min, name } = this.state;
-      return isLoading ? <Loading /> : <Weather temp={temp} condition={condition}/>;
+    this.setState({isLoding:false, condition:weather[0].main, temp, weatherStep});
+    
+  };
+  //expo getlocaion
+  getLocation = async() => {
+    //permission
+    try {
+      //const response = await Location.requestPermissionsAsync();
+      //console.log(responese)
+      //const location = await Location.getCurrentPositionAsync();
+      //const {coords} = locaion;
+      await Location.requestPermissionsAsync();
+      const {coords:{latitude, longitude}} = await Location.getCurrentPositionAsync();
+      console.log(latitude,longitude)
+      this.getWeather(latitude, longitude);
+      setTimeout(()=>{this.setState({isLoding: false})},1000);
+      
+      //console.log(location);
+      //throw Error()
+      // Send to API 
+    } catch (error) {
+      Alert.alert("Do not access permission", "Plz Agree permission");
+      console.log(error)
     }
     
-  
+  };
+  componentDidMount(){
+    this.getLocation();
+  }
+  render(){
+    const {isLoding, temp, condition, weatherStep} = this.state;
+    return isLoding ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition} weatherStep={weatherStep}/>;
+  }
 }
-
--- weather.js
+++ weather.js
@@ -1,29 +1,130 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {View, Text, StyleSheet, StatusBar }from "react-native";
 import PropTypes from "prop-types";
+import { LinearGradient } from "expo-linear-gradient";
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
 
-export default function Weather({temp, condition,temp_max, temp_min, name}){
-    return <View style={styles.container}>
-            <Text>{temp}</Text>
-            <Text></Text>
-            {/* <Text>{name}</Text>
-            <Text>{temp_max}</Text>
-            <Text>{temp_min}</Text> */}
-            </View>
+const outFit={
+    tooHot:{
+        wearing:["민소매", "반팔", "반바지", "원피스"]
+    },
+    hot:{
+        wearing:["반팔","얇은셔츠"]
+    },
+    tooWarm:{
+        wearing:["얇은가디건"]
+    },
+    warm:{wearing:["얇은니트"]},
+    tooAutumn:{wearing:["가디건","야상"]},
+    autumn:{wearing:["트렌치코트"]},
+    tooCold:{wearing:["코트","히트텍"]},
+    cold:{wearing:["패딩","목도리"]},
+
+}
+const weatherOptions = {
+    Haze: {
+      iconName: "weather-hazy",
+      gradient: ["#FFAF7B", "#D76D77","#3A1C71"]
+    },
+    Thunderstorm: {
+      iconName: "weather-lightning",
+      gradient: ["#283048","#859398"]
+    },
+    Drizzle: {
+      iconName: "weather-rainy",
+      gradient: ["#3D7EAA","#FFE47A"]
+    },
+    Rain: {
+      iconName: "weather-pouring",
+      gradient: ["#085078","#85D8CE"]
+    },
+    Snow: {
+      iconName: "weather-snowy-heavy",
+      gradient: ["#403B4A","#E7E9BB"]
+    },
+    Atmosphere: {
+      iconName: "weather-fog",
+      gradient: ["#757F9A","#D7DDE8"]
+    },
+    Clear: {
+      iconName: "weather-sunny",
+      gradient: ["#e1eec3","#f05053"]
+    },
+    Clouds: {
+      iconName: "weather-cloudy",
+      gradient: ["#757F9A","#D7DDE8"]
+    },
+    Mist: {
+      iconName: "weather-fog",
+      gradient: ["#757F9A","#D7DDE8"]
+    },
+    Dust: {
+      iconName: "weather-fog",
+      gradient: ["#757F9A","#D7DDE8"]
+    }
+  };
+export default function Weather({temp, condition, weatherStep}){
+    console.log(weatherStep);
+    console.log(condition);
+    return(
+    <LinearGradient colors={weatherOptions[condition].gradient} style={styles.container}>
+        <StatusBar barStyle="light-content"/>
+        <View style={styles.halfContainer}>
+            <MaterialCommunityIcons name={weatherOptions[condition].iconName} size={70} color="white" />
+            <Text style={styles.temp}>{temp}<Text style={styles.degree}>℃</Text></Text>    
+        </View>
+        <View style={styles.outFitZone}>
+            <Text style={styles.outFit}>Recomand OutFit</Text>
+    <Text style={styles.outFit}>{outFit[weatherStep].wearing[0]}</Text>
+        </View>
+    </LinearGradient>
+    );
+
 }
 
-Weather.propTypes = {
-    temp: PropTypes.number.isRequired,
-    condition: PropTypes.oneOf(["Thunderstorm", "Drizzle", "Rain", "Snow", "Atmosphere", "Clear", "Clouds"]).isRequired
-    // temp_min: PropTypes.number.isRequired,
-    // temp_max: PropTypes.number.isRequired,
-    // name: PropTypes.string.isRequired,
+Weather.propTypes={
+    temp:PropTypes.number.isRequired,
+    condition: PropTypes.oneOf([
+        "Thunderstorm",
+        "Drizzle",
+        "Rain",
+        "Snow",
+        "Atmosphere",
+        "Clear",
+        "Clouds",
+        "Haze",
+        "Mist",
+        "Dust"
+      ]).isRequired
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+    container:{
+        flex:1,
+    },
+    temp:{
+        fontSize: 40,
+        color:"white"
+    },
+    outFitZone:{
+        flex:3,
+        //backgroundColor: "red"
+    },
+    halfContainer:{
+        flex:2,
+        justifyContent:"center",
+        alignItems:"center",
+        //backgroundColor: "blue"
+    },
+    outFit:{
+        textAlign: "left",
+        fontWeight: "bold",
+        color:"white",
+        paddingLeft:35,
+        fontSize:20
+    },
+    degree:{
+        fontSize:25,
     }
+
 });
